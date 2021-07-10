00dfd4660925bd4087b3174264d5bfa981a0bbb0
EverthonCaldeira03 <caldeiraeverthon03@gmail.com>
2021-07-10 18:54:19 -0300

    Inicio da pagina post

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'test.html'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
-
+app.get('/post', (req, res) => {
+    res.sendFile(__dirname + '/post01.html');
+})
 app.listen(3000, () => {
     console.log()
     console.log('listening on port 3000')
